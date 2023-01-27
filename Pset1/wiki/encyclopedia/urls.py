eb5fc9c4f66bda9b5f7954ab7c41b85ca17d19e4
Chong-Tuc-Yee <107826905+Chong-Tuc-Yee@users.noreply.github.com>
2023-01-27 23:56:54 +0800

    Create urls.py

---
+++ Pset1/wiki/encyclopedia/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # Expect to receive a string from URL and use it in function in views.
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit/", views.save_edit, name="save_edit"),
+    path("rand/", views.rand, name="rand")
+]
